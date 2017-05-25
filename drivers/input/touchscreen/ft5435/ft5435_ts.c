7f8c6a548e95529932d5809656322ecbee032f8f
TheScarastic <warabhishek@gmail.com>
2017-05-25 13:00:49 +0000

    drivers: input: Stop ft5435 spam
    Signed-off-by: Albert I <kras@raphielgang.org>

--- drivers/input/touchscreen/ft5435/ft5435_ts.c
+++ drivers/input/touchscreen/ft5435/ft5435_ts.c
@@ -1197,10 +1197,6 @@ for (i = 0; i < data->pdata->num_max_touches; i++) {
 
 		if (!num_touches && !status && !id)
 			break;
-		if (status==FT_TOUCH_DOWN)
-			printk("[FTS]Down pid[%d]:[%d,%d]\n",id,x,y);
-		else if(status==1)
-			printk("[FTS]Up pid[%d]:[%d,%d]\n",id,x,y);
 		if ( x < data->pdata->panel_maxx && y < data->pdata->panel_maxy ) {
 			input_mt_slot(ip_dev, id);
 			if (status == FT_TOUCH_DOWN || status == FT_TOUCH_CONTACT) {
